8c48d29260f5ae7a49dcf5b55006c691192c21df
KOBOKO23 <kobokomasero@gmail.com>
2023-12-14 11:03:57 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - the Entry point
+ * main - Entry point
  *
- * Return always (0) - success
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
