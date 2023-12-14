8c53db0b2e1a4c4503f7eb074e9cfbaed72220a0
KOBOKO23 <kobokomasero@gmail.com>
2023-12-14 10:09:46 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return alays (0) - success
+ *
+ */
+
+int main()
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return 0;
+}
