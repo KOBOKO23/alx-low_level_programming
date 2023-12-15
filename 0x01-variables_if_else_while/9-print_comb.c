427ed864872ab56ad8978b90452695b27417f361
KOBOKO23 <kobokomasero@gmail.com>
2023-12-15 08:36:23 -0800

    9-print_comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,30 @@
+/*
+ * File: 9-print_comb.c
+ * Auth: Philip Koboko
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints all possible combinations of single-digit numbers.
+ *
+ * Return: Always 0 - (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar((i % 10) + '0');
+		if (i == 9)
+			continue;
+
+		putchar(',');
+		putchar(' ');
+	}
+
+	putchar('\n');
+
+	return (0);
+}
