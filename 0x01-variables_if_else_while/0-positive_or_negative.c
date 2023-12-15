1a37c115447b8a9acce729fc02a46f47abc70703
KOBOKO23 <kobokomasero@gmail.com>
2023-12-15 05:46:32 -0800

    0-positive_or_negative

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,32 @@
+/*
+ * File: 0-positive_or_negative.c
+ * Auth: Philip Koboko
+ */
+
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Assigns random number to n, determines whether
+ * the number is positive, negative or zero.
+ *
+ *
+ * Return: Always 0 - (success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
+	else
+		printf("%d is zero\n", n);
+
+	return (0);
+}
