74e18a713c7ee12ceba596db5da467078a31cd90
KOBOKO23 <kobokomasero@gmail.com>
2023-12-19 04:19:04 -0800

    10-add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  * add - Adds two numbers
+  * @a: first number
+  * @b: second number
+  *
+  * Return: Integer value
+  */
+int add(int a, int b)
+{
+	return (a + b);
+}
