5327d492c150676173440d1e6b5d01fcb7d597c3
KOBOKO23 <kobokomasero@gmail.com>
2023-12-21 03:21:43 -0800

    3-print_numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*print_numbers - prints all numbers from 0 to 9
+*
+*Return: returns void
+*/
+
+void print_numbers(void)
+{
+	int num = 48;
+	
+	for (; num < 58; num++)
+	{
+		_putchar(num);
+	}
+	_putchar(10);
+}
